da32b00ffb8d2afc2e63ada2245b495d8dcc460d
timrusin <timrusin@gmail.com>
2022-03-07 22:05:55 -0600

    messing with style in Mode need new uniform images

--- src/components/ModeComponent.js
+++ src/components/ModeComponent.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import { Text, Image, TouchableOpacity } from 'react-native';
+import { Text, View, Image, TouchableOpacity, StyleSheet } from 'react-native';
 
 
 const ModeComponent = ({navigation, link, imageSource, title}) => {
     console.log(navigation)
 
   return (
-      <TouchableOpacity onPress={()=> navigation.navigate(link)} >
-        <Image source={imageSource} style={{height:100, width:100}}/>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.navigate(link)}>
+        <Image style={styles.images} source={imageSource} />
         <Text>{title}</Text>
       </TouchableOpacity>
+    </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container:{
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: 'purple',
+  },
+
+  images:{
+    textAlign: 'center',
+    width: 170,
+    height: 180,
+    marginTop: 30,
+  }
+})
 
 export default ModeComponent;
