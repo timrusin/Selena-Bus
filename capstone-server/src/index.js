f054b6b50646edf0112462066df8a007a1e86b9d
timrusin <timrusin@gmail.com>
2022-03-11 15:30:45 -0600

    backend created with post and get working

--- capstone-server/src/index.js
+++ capstone-server/src/index.js
@@ -1,15 +1,17 @@
+require('dotenv').config()
 require('./models/Score')
 const express = require ('express');
 const mongoose = require ('mongoose')
 const bodyParser = require('body-parser')
 const scoreRoutes = require('./routes/scoreRoutes')
 
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(scoreRoutes);
 
-const mongoUri = 'mongodb+srv://timrusin:6217CodingSelena!@cluster0.s5ty2.mongodb.net/Salena-Bus?retryWrites=true&w=majority'
+const mongoUri = process.env.DATABASE_URL
 mongoose.connect(mongoUri)
 
 mongoose.connection.on('connected', ()=> {
